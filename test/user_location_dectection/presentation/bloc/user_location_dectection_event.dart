e48549b592830a3f867bab0197b1c0342bd59c5d
Erik Jensen <eje@westbit.dk>
2023-10-02 15:12:31 +0200

    Created test folder structure

---
+++ test/user_location_dectection/presentation/bloc/user_location_dectection_event.dart
@@ -0,0 +1,8 @@
+part of 'user_location_dectection_bloc.dart';
+
+abstract class UserLocationDectectionEvent extends Equatable {
+  const UserLocationDectectionEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ test/user_location_dectection/presentation/bloc/user_location_dectection_state.dart
@@ -0,0 +1,9 @@
+part of 'user_location_dectection_bloc.dart';
+
+abstract class UserLocationDectectionState extends Equatable {
+  const UserLocationDectectionState();  
+
+  @override
+  List<Object> get props => [];
+}
+class UserLocationDectectionInitial extends UserLocationDectectionState {}
